834e84a25bbf0a476ad98593826cfa2f42a2901f
Tatiana Sambueva <tatiana.sambueva@gmail.com>
2024-02-06 22:29:47 +0200

    price interval calculations

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import { useState } from 'react';
-import './App.scss';
-import Container from 'react-bootstrap/Container';
-import Body from './Body';
-import Head, { DEFAULT_ACTIVE_BUTTON } from './Head';
-import Footer from './Footer';
-import LeftSideBar from './LeftSideBar';
-import { getDefaultFrom, getDefaultUntil } from './utils/dates';
-
+import { useState } from "react";
+import "./App.scss";
+import Container from "react-bootstrap/Container";
+import Body from "./Body";
+import Head, { DEFAULT_ACTIVE_BUTTON } from "./Head";
+import Footer from "./Footer";
+import LeftSideBar from "./LeftSideBar";
+import { getDefaultFrom, getDefaultUntil } from "./utils/dates";
 
 function App() {
   const [activePrice, setActivePrice] = useState(DEFAULT_ACTIVE_BUTTON);
-  const [activeHour, setActiveHour] = useState();
+  const [activeHour, setActiveHour] = useState(1);
   const [showSideBar, setShowSideBar] = useState(false);
   const [from, setFrom] = useState(getDefaultFrom());
   const [until, setUntil] = useState(getDefaultUntil());
 
-
-
   const handleCloseSideBar = () => setShowSideBar(false);
   const handleOpenSideBar = () => setShowSideBar(true);
-
-
 
   return (
     <Container>
@@ -29,10 +24,7 @@ function App() {
         setActivePrice={setActivePrice}
         handleOpenSideBar={handleOpenSideBar}
       />
-      <Body
-        activeHour={activeHour}
-        from={from}
-        until={until} />
+      <Body activeHour={activeHour} from={from} until={until} />
       <Footer
         activePrice={activePrice}
         activeHour={activeHour}
@@ -44,9 +36,9 @@ function App() {
         from={from}
         until={until}
         setFrom={setFrom}
-        setUntil={setUntil} />
+        setUntil={setUntil}
+      />
     </Container>
-
   );
 }
 
