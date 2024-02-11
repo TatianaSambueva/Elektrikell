fb735112447aed3323808f97051d063179dd83bd
Tatiana Sambueva <tatiana.sambueva@gmail.com>
2024-02-11 22:24:11 +0200

    try to delete info fro

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,70 @@
-import { Routes, Route } from "react-router-dom";
-import ElectricPrice from "./ElectricPrice.";
-import About from "./About";
-import Navigation from "./Navigation";
+import { useState, useEffect } from "react";
+import "./App.scss";
+import Container from "react-bootstrap/Container";
+import Body from "./Body";
+import Head, { DEFAULT_ACTIVE_BUTTON } from "./Head";
+import Footer from "./Footer";
+import LeftSideBar from "./LeftSideBar";
+import { getDefaultFrom, getDefaultUntil } from "./utils/dates";
+import ErrorModal from "./ErrorModal";
+import { useParams } from "react-router-dom";
 
 function App() {
-    return (
-        <>
-            <Navigation />
-            <Routes>
-                <Route path="/" element={<ElectricPrice />}>
-                    <Route path="lowprice/:hours" element={<ElectricPrice />} />
-                </Route>
-                <Route path="/about" element={<About />}>
-                    <Route path=":id" element={<About />} />
-                    <Route path=":id/:name" element={<About />} />
-                </Route>
+  const params = useParams();
 
-                <Route path="*" element={<h1>404</h1>} />
-            </Routes>;
-        </>
-    );
-};
+  const [activePrice, setActivePrice] = useState(DEFAULT_ACTIVE_BUTTON);
+  const [activeHour, setActiveHour] = useState(1);
+  const [showSideBar, setShowSideBar] = useState(false);
+  const [from, setFrom] = useState(getDefaultFrom());
+  const [until, setUntil] = useState(getDefaultUntil());
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [bestUntil, setBestUntil] = useState(0);
+
+  const handleCloseSideBar = () => setShowSideBar(false);
+  const handleOpenSideBar = () => setShowSideBar(true);
+
+
+  useEffect(() => {
+    if (params.hours) setActiveHour(+params.hours);
+  }, [params]);
+
+
+  return (
+    <Container>
+      <Head
+        activePrice={activePrice}
+        setActivePrice={setActivePrice}
+        handleOpenSideBar={handleOpenSideBar}
+        setErrorMessage={setErrorMessage}
+      />
+      <Body
+        activeHour={activeHour}
+        from={from}
+        until={until}
+        setErrorMessage={setErrorMessage}
+        setBestUntil={setBestUntil}
+      />
+      <Footer
+        activePrice={activePrice}
+        activeHour={activeHour}
+        setActiveHour={setActiveHour}
+        bestUntil={bestUntil}
+      />
+      <LeftSideBar
+        show={showSideBar}
+        handleClose={handleCloseSideBar}
+        from={from}
+        until={until}
+        setFrom={setFrom}
+        setUntil={setUntil}
+      />
+      <ErrorModal
+        show={!!errorMessage}
+        handleClose={() => setErrorMessage(null)}
+        errorMessage={errorMessage}
+      />
+    </Container>
+  );
+}
 
 export default App;
