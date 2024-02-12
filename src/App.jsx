6005aa44c1b7c1668d7e98d5b0811e119c088112
Tatiana Sambueva <tatiana.sambueva@gmail.com>
2024-02-12 13:23:23 +0200

    infro about project, me and gamma. Links added.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import ElectricPrice from "./ElectricPrice.";
 import About from "./About";
 import Navigation from "./Navigation";
+import AboutMeAndGamma from "./About/AboutMeAndGamma";
 
 function App() {
     return (
@@ -11,10 +12,10 @@ function App() {
                 <Route path="/" element={<ElectricPrice />}>
                     <Route path="lowprice/:hours" element={<ElectricPrice />} />
                 </Route>
-                <Route path="/about" element={<About />}>
-                    <Route path=":id" element={<About />} />
-                    <Route path=":id/:name" element={<About />} />
-                </Route>
+                <Route path="/about" element={<About />} />
+                <Route path="/about/me" element={<AboutMeAndGamma info="me" />} />
+                <Route path="/about/gamma" element={<AboutMeAndGamma info="gamma" />} />
+
 
                 <Route path="*" element={<h1>404</h1>} />
             </Routes>;
